04a17c562f34ef159f5d80e6f5759d86da023ee6
voskoboenko <andreyhtml1997@gmail.com>
2018-08-21 01:00:36 +0300

    second commit

---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.js
+//= ../../bower_components/jquery-mask-plugin/dist/jquery.mask.min.js
+//= ../../bower_components/owl.carousel/dist/owl.carousel.min.js
